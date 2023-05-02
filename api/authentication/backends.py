b1954e2bbcb62c99ce555339b1899799143b6f13
JesusCantuR <jesus.cantu@introid.com>
2023-05-02 13:22:33 -0600

    Fix function being called as object.

--- api/authentication/backends.py
+++ api/authentication/backends.py
@@ -20,7 +20,7 @@ class AllAuthAuthenticationBackend(AllAuthAuthenticationBackendBase):
     
     def get_user(self, user_id):
         try:
-            user = UserModel._default_manager.select_related.get(pk=user_id)
+            user = UserModel._default_manager.get(pk=user_id)
         except UserModel.DoesNotExist:
             return None
         return user if self.user_can_authenticate(user) else None
