b4ac20bc6513b267883d0d850bf1ad9cdd301b99
JesusCantuR <jesus.cantu@introid.com>
2023-05-22 14:01:31 -0600

    Minor comments change.

--- api/authentication/models.py
+++ api/authentication/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class User(AbstractUser):
-    pass
+# https://docs.djangoproject.com/en/4.2/topics/auth/customizing/#substituting-a-custom-user-model
 
 #! Prefer the get_user_model() method over importing the User model directly:
 # from django.contrib.auth import get_user_model
 # UserModel = get_user_model()
 
-#! Prefer using the AUTH_USER_MODEL setting over a direct import of User for foreign keys:
+#! Prefer using the AUTH_USER_MODEL setting over a direct import of User 
+#! for foreign keys or code executed at import time:
 # from django.conf import settings
 # settings.AUTH_USER_MODEL
+
+class User(AbstractUser):
+    pass
