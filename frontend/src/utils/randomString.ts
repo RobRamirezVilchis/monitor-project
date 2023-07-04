796f177a6b5e9b2c3ab3b326ebc0c8aeffb6198e
JesusCantuR <jesus.cantu@introid.com>
2023-07-03 18:00:06 -0600

    Add random string generator.

---
+++ frontend/src/utils/randomString.ts
@@ -0,0 +1,10 @@
+// https://stackoverflow.com/a/10727155
+export function randomString(
+  length: number,
+  chars: string = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+) {
+  var result = "";
+  for (var i = length; i > 0; --i)
+    result += chars[Math.floor(Math.random() * chars.length)];
+  return result;
+}
