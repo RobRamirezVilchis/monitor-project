2bf504f857e8c50445200c0d891f00de7daafc57
JesusCantuR <jesus.cantu@introid.com>
2023-08-07 18:00:12 -0600

    Add standarized error messages for the api.

---
+++ api/common/exception_formatter.py
@@ -0,0 +1,13 @@
+from drf_standardized_errors.formatter import ExceptionFormatter as _ExceptionFormatter
+from drf_standardized_errors.types import ErrorResponse
+
+class ExceptionFormatter(_ExceptionFormatter):
+    def format_error_response(self, error_response: ErrorResponse):
+        return {
+            "type": error_response.type,
+            "errors": [{
+                "code": error.code,
+                "detail": error.detail,
+                "field": error.attr
+            } for error in error_response.errors]
+        }
