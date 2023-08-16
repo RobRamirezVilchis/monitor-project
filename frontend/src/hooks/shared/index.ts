9604b2159b66fdab2073f699137ff5bd23f983f3
JesusCantuR <jesus.cantu@introid.com>
2023-08-16 16:59:52 -0600

    Update shared components/hooks exports.

--- frontend/src/hooks/shared/index.ts
+++ frontend/src/hooks/shared/index.ts
@@ -1,3 +1,4 @@
+export { useConfirmDialog, type UseConfirmDialogOptions } from "./useConfirmDialog";
 export { useDebounce } from "./useDebounce";
 export { useMediaQuery } from "./useMediaQuery";
 export { useQueryState } from "./useQueryState";
