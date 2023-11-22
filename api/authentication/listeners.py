e59cb03f94395dcf92855c2d1a9603e65c3012a5
JesusCantuR <jesus.cantu@introid.com>
2023-11-22 16:54:31 -0600

    Remove google oauth token revoke in user log out since it would affect other sessions.

--- api/authentication/listeners.py
+++ api/authentication/listeners.py
@@ -1,7 +1,6 @@
 from allauth.account.signals import user_logged_in, user_logged_out
 from allauth.socialaccount.models import SocialAccount, SocialToken
 from django.dispatch import receiver
-import requests
 
 
 @receiver(user_logged_in)
@@ -15,14 +14,3 @@ def on_user_logged_in(sender, request, user, **kwargs):
     else:
         # Set session expiration to 24 hours
         request.session.set_expiry(24 * 60 * 60)
-
-
-@receiver(user_logged_out)
-def on_user_logged_out(sender, request, user, **kwargs):
-    try:
-        token = SocialToken.objects.get(account__user=user, account__provider='google')
-        access_token = token.token
-        if access_token:
-            requests.get(f"https://accounts.google.com/o/oauth2/revoke?token={access_token}")
-    except Exception as e:
-        pass
