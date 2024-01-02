92d780c04f344f0c535d111bfb63ac045eea1db7
JesusCantuR <jesus.cantu@introid.com>
2024-01-02 12:17:10 -0600

    Update local default database to only set the default database instead of overwritting the whole DATABASES config object.

--- api/config/django/local.py
+++ api/config/django/local.py
@@ -8,10 +8,11 @@ CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
 
 AUTH_PASSWORD_VALIDATORS = []
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
+DATABASES["default"] = {
+    "ENGINE": "django.db.backends.sqlite3",
+    "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    "OPTIONS": {
+        "timeout": 20,
+    },
 }
 
