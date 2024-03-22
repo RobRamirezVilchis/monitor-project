8098e643c5946d27c89b55e66350d6dece2e9f0b
spare <roberto.ramirez@introid.com>
2024-03-22 13:58:15 -0600

    Fix settings file

--- api/config/django/local.py
+++ api/config/django/local.py
@@ -4,7 +4,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "192.168.1.173"]  # ["*"]
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost:3000", "192.168.1.173"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000", "http://192.168.1.173"]
 
 AUTH_PASSWORD_VALIDATORS = []
 
