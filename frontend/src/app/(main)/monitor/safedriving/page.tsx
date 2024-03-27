2ba5a6dd07e2779252ff931c4585d048468bdb6d
spare <roberto.ramirez@introid.com>
2024-03-27 17:18:02 -0600

    Update mantine, add pie chart

--- frontend/src/app/(main)/monitor/safedriving/page.tsx
+++ frontend/src/app/(main)/monitor/safedriving/page.tsx
@@ -12,6 +12,7 @@ import {
   Select,
   ComboboxItem,
 } from "@mantine/core";
+import { PieChart } from "@mantine/charts";
 
 import {
   useUnitsQuery,
@@ -41,6 +42,14 @@ const statusNames: { [key in StatusKey]: string } = {
   3: "Alerta",
   4: "Fallando",
   5: "Crítico",
+};
+const statusColors: { [key in StatusKey]: string } = {
+  0: "gray",
+  1: "blue",
+  2: "green",
+  3: "yellow",
+  4: "orange",
+  5: "red",
 };
 
 const SafeDrivingPage = () => {
@@ -77,73 +86,108 @@ const SafeDrivingPage = () => {
   const clients = clientsQuery.data?.map((data) => data.name);
 
   const unitData = unitsStatusQuery.data;
+  const pieData = [
+    { name: "Crítico", value: 180, color: "yellow.6" },
+    { name: "Funcionando", value: 500, color: "indigo.6" },
+  ];
+  const data = [];
+  //for (let i = 0, i < countQuery.data?.length, )
+  if (countQuery.data) {
+    for (const level of countQuery?.data) {
+      if (level["severity"] != 0) {
+        data.push({
+          name: statusNames[level["severity"] as StatusKey],
+          value: level["count"],
+          color: statusColors[level["severity"] as StatusKey],
+        });
+      }
+    }
+  }
 
   return (
     <section>
-      <div className="flex items-center">
-        <h1 className="text-5xl font-bold pb-2 flex-1 mb-3 md:mb-6 pr-2">
-          Safe Driving
-        </h1>
-        <ColorSchemeSwitchToggle />
-      </div>
+      <div className="flex">
+        <div className="mr-10">
+          <div className="flex justify-start items-center pb-2  mb-3 md:mb-6">
+            <h1 className="text-5xl font-bold  pr-10">Safe Driving</h1>
+          </div>
 
-      <div className="flex flex-col md:flex-row mb-4 gap-4 md:gap-10">
-        <TextInput
-          className="md:flex gap-3 items-center "
-          styles={{
-            label: { fontSize: 18 },
-          }}
-          label="Buscar unidad:"
-          value={value}
-          onChange={(event) => setValue(event.currentTarget.value)}
-        />
+          <div className="flex flex-col md:flex-row mb-4 gap-4 md:gap-10">
+            <TextInput
+              className="md:flex gap-3 items-center "
+              styles={{
+                label: { fontSize: 18 },
+              }}
+              label="Buscar unidad:"
+              value={value}
+              onChange={(event) => setValue(event.currentTarget.value)}
+            />
 
-        <Select
-          className="md:flex gap-3 items-center"
-          styles={{
-            label: { fontSize: 18 },
-          }}
-          label="Filtrar por cliente:"
-          placeholder="Todos"
-          data={clients}
-          onChange={(value: string | null) => setClientValue(value)}
-        ></Select>
-      </div>
+            <Select
+              className="md:flex gap-3 items-center"
+              styles={{
+                label: { fontSize: 18 },
+              }}
+              label="Filtrar por cliente:"
+              placeholder="Todos"
+              data={clients}
+              onChange={(value: string | null) => setClientValue(value)}
+            ></Select>
+          </div>
 
-      {countQuery.data && (
-        <div className="flex w-fit py-2 mb-4 gap-6 flex-wrap">
-          {countQuery.data.map((severity_count) => (
-            <div
-              key={severity_count.severity}
-              className="flex gap-2 items-center"
-            >
-              <p>{severity_count.count}</p>
-              <p>-</p>
+          {countQuery.data && (
+            <div className="flex w-fit py-2 mb-4 gap-6 flex-wrap">
+              {countQuery.data.map((severity_count) => (
+                <div
+                  key={severity_count.severity}
+                  className="flex gap-2 items-center"
+                >
+                  <p>{severity_count.count}</p>
+                  <p>-</p>
 
-              <Link
-                href={
-                  filter == null || Number(filter) != severity_count.severity
-                    ? "/monitor/safedriving/?" +
-                      createQueryString(
-                        "filter",
-                        String(severity_count.severity)
-                      )
-                    : "/monitor/safedriving/"
-                }
-                className={`${
-                  severity_count.severity == Number(filter) || filter == null
-                    ? "opacity-100"
-                    : "opacity-30"
-                } inline-flex px-2.5 pt-1 pb-0.5 text-s font-semibold border-2 ${
-                  statusStyles[severity_count.severity as StatusKey]
-                } rounded-full`}
-              >
-                {statusNames[severity_count.severity as StatusKey]}
-              </Link>
+                  <Link
+                    href={
+                      filter == null ||
+                      Number(filter) != severity_count.severity
+                        ? "/monitor/safedriving/?" +
+                          createQueryString(
+                            "filter",
+                            String(severity_count.severity)
+                          )
+                        : "/monitor/safedriving/"
+                    }
+                    className={`${
+                      severity_count.severity == Number(filter) ||
+                      filter == null
+                        ? "opacity-100"
+                        : "opacity-30"
+                    } inline-flex px-2.5 pt-1 pb-0.5 text-s font-semibold border-2 ${
+                      statusStyles[severity_count.severity as StatusKey]
+                    } rounded-full`}
+                  >
+                    {statusNames[severity_count.severity as StatusKey]}
+                  </Link>
+                </div>
+              ))}
             </div>
-          ))}
+          )}
         </div>
-      )}
+        <div className="hidden md:block">
+          <PieChart
+            data={data}
+            mt={0}
+            mb={0}
+            py={0}
+            size={150}
+            withLabels
+            withLabelsLine
+            labelsType="percent"
+            labelsPosition="outside"
+            withTooltip
+            tooltipDataSource="segment"
+          />
+        </div>
+      </div>
 
       <div className="flex flex-row gap-4 flex-wrap">
         {unitData?.map((unit) =>
