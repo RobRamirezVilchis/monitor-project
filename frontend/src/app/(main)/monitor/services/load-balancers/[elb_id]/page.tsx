75079f84138c9806b2fb165985f4941ea65397c7
spare <roberto.ramirez@introid.com>
2024-05-30 15:16:26 -0600

    Improve load balancer page UI

--- frontend/src/app/(main)/monitor/services/load-balancers/[elb_id]/page.tsx
+++ frontend/src/app/(main)/monitor/services/load-balancers/[elb_id]/page.tsx
@@ -13,7 +13,7 @@ import {
   PaymentOutlined,
   Spa,
 } from "@mui/icons-material";
-import { Progress, SegmentedControl } from "@mantine/core";
+import { Progress, SegmentedControl, Skeleton } from "@mantine/core";
 import { useDataGrid, useSsrDataGrid } from "@/hooks/data-grid";
 import {
   ServerHistory,
@@ -156,11 +156,10 @@ const LoadBalancerPage = ({ params }: { params: { elb_id: string } }) => {
   if (metricsKeys) {
     metrics = Object.values(metricsKeys);
   }
-
   let splitter = new RegExp("_|-", "g");
 
   return (
-    <section className="relative mb-28">
+    <section className="relative mb-20">
       <BackArrow />
       <div className="flex justify-between items-center">
         <h1 className="mb-6 text-5xl font-bold pr-10">
@@ -172,8 +171,8 @@ const LoadBalancerPage = ({ params }: { params: { elb_id: string } }) => {
           )}
         </h1>
       </div>
-      {elbStatus && (
-        <div className="text-2xl text-gray-500">
+      {elbStatus ? (
+        <div className="text-xl text-gray-500">
           <p>
             Última actividad:{" "}
             {format(parseISO(String(elbStatus.last_activity)), "Pp")}
@@ -200,20 +199,32 @@ const LoadBalancerPage = ({ params }: { params: { elb_id: string } }) => {
             </div>
           )}
         </div>
+      ) : (
+        <div className="space-y-2">
+          <Skeleton h={20} w={300} />
+          <Skeleton h={20} w={300} />
+          <Skeleton h={20} w={300} />
+        </div>
       )}
-      <div className="h-[62vh] mb-10">
-        <DataGrid instance={grid} />
-      </div>
-      <p className="text-2xl opacity-60 mb-2 pt-10">Gráfica de métricas</p>
-      <div className="md:flex items-center gap-8 mb-4">
+
+      <h2 className="text-3xl opacity-60 mb-2 mt-8">Gráfica de métricas</h2>
+      <div className="md:flex items-center gap-8 mb-4 justify-between">
         <div className="mt-1 sm:mt-0">
+          {metrics.length == 0 && (
+            <Skeleton visible={metrics.length == 0} h={30} w={300} />
+          )}
           <SegmentedControl
             value={plotMetric}
             onChange={setPlotMetric}
             data={metrics}
+            classNames={{
+              root: "bg-gray-200 rounded-xl",
+              indicator: "rounded-lg",
+            }}
           ></SegmentedControl>
         </div>
-        <div className="w-80 mt-1 sm:mt-0">
+        <div className="sm:flex w-96 gap-2 items-center mt-1 sm:mt-0">
+          <p>Rango de tiempo:</p>
           <DatePickerInput
             type="range"
             placeholder="Pick date"
@@ -222,21 +233,25 @@ const LoadBalancerPage = ({ params }: { params: { elb_id: string } }) => {
           />
         </div>
       </div>
-      {plotData && (
+      <Skeleton visible={plotData == undefined}>
         <LineChart
           tooltipProps={{
             content: ({ label, payload }) => (
               <ChartTooltip label={label} payload={payload} />
             ),
           }}
-          h={500}
+          h={450}
           data={plotData as Record<string, any>[]}
           dataKey="register_datetime"
           series={[{ name: "metric_value", color: "green.6" }]}
           curveType="linear"
           withDots={false}
         ></LineChart>
-      )}
+      </Skeleton>
+      <h2 className="text-3xl opacity-60 mb-2 mt-28">Listado de métricas</h2>
+      <div className="h-[70vh] mt-5">
+        <DataGrid instance={grid} />
+      </div>
     </section>
   );
 };
